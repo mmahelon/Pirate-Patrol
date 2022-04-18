cd420ab26cf7c28238dbed7fec87008924f174ce
mmahelon <mmahelon@ucsc.edu>
2022-04-17 18:42:58 -0700

    'Final Push'

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+//Micah Mahelona, 4/17/2022
+//Pirate Patrol, 18 hrs
+
+//Point Breakdown
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+//-Implement a new timing/scoring mechanism that adds time to the clock for successful hits (20)
+//-Display the time remaining (in seconds) on the screen (10)
+//-Implement the 'FIRE' UI text from the original game (5)
+//-Add your own (copyright-free) background music to the Play scene (5)
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
